436ea0c76296a49a6a36200decd639f3fb179f2c
mittz333 <mittz333@gmail.com>
2021-01-11 10:19:29 +0900

    Rubocopチェック&不要コメント削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,17 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birthday, presence: true
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :first_name
     validates :last_name
   end
-  
+
   with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角文字（カタカナ）を使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
-
 end
