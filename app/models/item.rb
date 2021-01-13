f9af5a0db170c5efa1aea9bd864a295a974320dc
mittz333 <mittz333@gmail.com>
2021-01-13 17:29:27 +0900

    商品出品時の基本的な出品可能条件の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
   validates :shipping_charge_id, numericality: { other_than: 0 } 
   validates :prefecture_id, numericality: { other_than: 0 } 
   validates :shipping_day_id, numericality: { other_than: 0 }
-  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999 }
   validates :image, presence: true
 
 end
