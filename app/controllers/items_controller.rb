2688d6b5900388f8006d980b0d69702da5c4697f
mittz333 <mittz333@gmail.com>
2021-01-13 13:58:00 +0900

    ActiveStorage関係導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,22 @@ class ItemsController < ApplicationController
     end
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    # binding.pry
+    params.require(:item).permit(:title, :detail, :category_id, :condition_id, :shipping_charge_id, :price, :image)
+        .merge(user_id: current_user.id)
+  end
+
 end
