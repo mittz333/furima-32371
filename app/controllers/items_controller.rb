2f3e491772bef2d8c7cb54574797576525707750
mittz333 <mittz333@gmail.com>
2021-01-13 09:58:04 +0900

    itemsテーブルのアソシエーションとバリデーション登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if !user_signed_in?
+      redirect_to root_path
+    end
+    @item = Item.new
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchase
+  has_many :comments ,dependent: :destroy
+
+  validates :title, presence: true
+  validates :detail, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_days_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
 end
