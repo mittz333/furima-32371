b7c70671bc94694d96d899dfe6f9e16619132f57
mittz333 <mittz333@gmail.com>
2021-01-20 12:19:57 +0900

    同一コントローラー内における記述のまとめ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.order(created_at: 'DESC')
@@ -19,11 +20,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
   end
 
@@ -43,5 +42,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:title, :detail, :category_id, :condition_id, :shipping_charge_id,
                                  :prefecture_id, :shipping_day_id, :price, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
