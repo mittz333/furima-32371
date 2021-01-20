8f815d01ad87c5b876ef113920d31da6c3652b7a
mittz333 <mittz333@gmail.com>
2021-01-20 14:43:00 +0900

    itemsコントローラーの調整

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :destroy]
 
   def index
     @items = Item.order(created_at: 'DESC')
@@ -37,8 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
+    @item.destroy if user_signed_in? && current_user.id == @item.user_id
     redirect_to root_path
   end
 
