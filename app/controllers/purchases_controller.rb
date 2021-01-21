20f18de9d43753f489ba45b4183b0375a07021ca
mittz333 <mittz333@gmail.com>
2021-01-22 08:57:16 +0900

    purchaseコントローラ作成(とりあえず)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,8 @@
 class PurchasesController < ApplicationController
+
+  private
+  def purchase_params
+    params.require(:purcase).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
