e71d06fdceaa5661235b4b564f2a6102049e0eae
mittz333 <mittz333@gmail.com>
2021-01-30 17:20:45 +0900

    バリデーション追加(正規表現)

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -4,11 +4,15 @@ class PurchaseForm
   attr_accessor :item_id, :user_id, :zipcode, :prefecture_id, :city, :street_number, :building, :tel, :purchase
 
   with_options presence: true do
-    validates :zipcode
     validates :city
     validates :street_number
+  end
+
+  with_options presence: true, format: { with: /\A\d{3}[-]\d{4}\z/, message: '郵便番号は「-」を含む且つ7桁）を使用してください' } do
+    validates :zipcode
+  end
+  with_options presence: true, format: { with: \d{,11}, message: '電話番号にはハイフンは不要で、11桁以内' } do
     validates :tel
-    validates :street_number
   end
 
   with_options numericality: { other_than: 0 } do
