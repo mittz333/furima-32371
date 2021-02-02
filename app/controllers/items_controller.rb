bc8dfa5c1f2d04c52320472a50dd42e009c64fff
mittz333 <mittz333@gmail.com>
2021-02-02 19:40:45 +0900

    購入済後に商品編集画面に直接でも跳べなくする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to root_path unless user_signed_in? && current_user.id == @item.user_id && @item.purchase == nil
   end
 
   def update
@@ -52,4 +52,5 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+  
 end
